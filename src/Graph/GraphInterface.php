2951a38790e46efcdf2764e2794ecbb846ef5620
Nilo Soares <nilosoares@gmail.com>
2020-10-14 16:37:07 -0300

    Update argument names and docblocks

--- src/Graph/GraphInterface.php
+++ src/Graph/GraphInterface.php
@@ -18,60 +18,60 @@ interface GraphInterface
     public function getEdges(): array;
 
     /**
-     * Return the indegree of a node (number of incoming edges).
-     * @param NodeInterface $node
+     * Return the in degree of a node U (number of incoming edges).
+     * @param NodeInterface $u
      * @return int
      */
-    public function getIndegree(NodeInterface $node): int;
+    public function getIndegree(NodeInterface $u): int;
 
     /**
      * Test if a node has incoming edges.
-     * @param NodeInterface $node
+     * @param NodeInterface $u
      * @return bool
      */
-    public function hasIncomingEdges(NodeInterface $node): bool;
+    public function hasIncomingEdges(NodeInterface $u): bool;
 
     /**
-     * List all edges going to node A.
-     * @param NodeInterface $node
+     * List all edges going to node U.
+     * @param NodeInterface $u
      * @return EdgeInterface[]
      */
-    public function getIncomingEdges(NodeInterface $node): array;
+    public function getIncomingEdges(NodeInterface $u): array;
 
     /**
-     * Return the outdegree of a node (number of outgoing edges).
-     * @param NodeInterface $node
+     * Return the out degree" of a node U (number of outgoing edges).
+     * @param NodeInterface $u
      * @return int
      */
-    public function getOutDegree(NodeInterface $node): int;
+    public function getOutDegree(NodeInterface $u): int;
 
     /**
      * Test if a node has outgoing edges.
-     * @param NodeInterface $node
+     * @param NodeInterface $u
      * @return bool
      */
-    public function hasOutgoingEdges(NodeInterface $node): bool;
+    public function hasOutgoingEdges(NodeInterface $u): bool;
 
     /**
-     * List all edges going out from node A.
-     * @param NodeInterface $node
+     * List all edges going out from node U.
+     * @param NodeInterface $u
      * @return array
      */
-    public function getOutgoingEdges(NodeInterface $node): array;
+    public function getOutgoingEdges(NodeInterface $u): array;
 
     /**
-     * List all nodes B such that there is an edge between A and B.
-     * @param NodeInterface $node
+     * List all nodes V such that there is an edge between U and V.
+     * @param NodeInterface $u
      * @return NodeInterface[]
      */
-    public function getNeighbors(NodeInterface $node): array;
+    public function getNeighbors(NodeInterface $u): array;
 
     /**
-     * Test if there is an edge from node A to the node B,
-     * @param NodeInterface $a
-     * @param NodeInterface $b
+     * Test if there is an edge from node U to the node V.
+     * @param NodeInterface $u
+     * @param NodeInterface $v
      * @return bool
      */
-    public function isAdjacent(NodeInterface $a, NodeInterface $b): bool;
+    public function isAdjacent(NodeInterface $u, NodeInterface $v): bool;
 
 }
