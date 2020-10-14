73a6d361e45acb4f06baada7554ff02610d94274
Nilo Soares <nilosoares@gmail.com>
2020-10-14 07:46:53 -0300

    Add the CPF value object (BrDoc)

---
+++ src/Docs/BrDocs/Cpf.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Nscps\Ds\Docs\BrDocs;
+
+class Cpf
+{
+
+    const PATTERN = '/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/';
+	const RAW_PATTERN = '/^[0-9]{11}$/';
+
+    /**
+     * @var string
+     */
+	private string $cpf = '';
+
+    /**
+     * Cpf constructor.
+     * @param string $cpf
+     */
+	public function __construct(string $cpf)
+	{
+		$this->validate($cpf);
+
+		$this->cpf = $cpf;
+	}
+
+    /**
+     * @return string
+     */
+	public function __toString()
+    {
+        return $this->getCpf();
+    }
+
+    /**
+     * @return string
+     */
+	public function getCpf(): string
+	{
+	    $raw = $this->getRaw();
+
+        return sprintf(
+            '%s.%s.%s-%s',
+            substr($raw, 0, 3),
+            substr($raw, 3, 3),
+            substr($raw, 6, 3),
+            substr($raw, 9, 2)
+        );
+	}
+
+    /**
+     * @return string
+     */
+	public function getRaw(): string
+	{
+		return preg_replace("/[^0-9]/", "", $this->cpf);
+	}
+
+    /**
+     * @param string $cpf
+     * @return int
+     */
+	private function getC1(string $cpf): int
+	{
+		$c1 = 0;
+		
+		$i = 0;
+		$j = 10;
+		while ($i <= 8) {
+			$c1 += $cpf[$i] * $j;
+			$i++;
+			$j--;
+		}
+		
+		return $c1;
+	}
+
+    /**
+     * @param string $cpf
+     * @return int
+     */
+	private function getC2(string $cpf): int
+	{
+		$c2 = 0;
+		
+		$i = 0;
+		$j = 11;
+		while ($i <= 8) {
+			$c2 += $cpf[$i] * $j;
+			$i++;
+			$j--;
+		}
+		
+        $c2 += $cpf[$i] * $j;
+		
+		return $c2;
+	}
+
+    /**
+     * @param int $c
+     * @return int
+     */
+	private function getDigit($c): int
+	{
+		return $c % 11 < 2 ? 0 : 11 - ($c % 11);
+	}
+
+    /**
+     * @param string $cpf
+     */
+	private function validate(string $cpf)
+	{
+		if (
+			!preg_match(self::RAW_PATTERN, $cpf) &&
+			!preg_match(self::PATTERN, $cpf)
+		) {
+			throw new \InvalidArgumentException('The CPF must contains 11 digits or match the following pattern: "999.999.999-99".');
+		}
+		
+		// Remove everything that is not a digit
+        $cpf = preg_replace("/[^0-9]/", "", $cpf);
+		
+		// 000.000.000-00, 111.111.111-11, ..., 999.999.999-99 are invalid.
+		for ($i = 0; $i <= 9; $i++) {
+			if (str_repeat((string) $i, 11) === $cpf) {
+				throw new \InvalidArgumentException('Invalid CPF.');
+			}
+		}
+		
+		// Validate D1
+        $c1 = $this->getC1($cpf);
+		$d1 = $this->getDigit($c1);
+		if ($d1 != $cpf[9]) {
+			throw new \InvalidArgumentException('Invalid CPF.');
+		}
+		
+		// Validate D2
+        $c2 = $this->getC2($cpf);
+		$d2 = $this->getDigit($c2);
+		if ($d2 != $cpf[10]) {
+			throw new \InvalidArgumentException('Invalid CPF.');
+		}
+	}
+	
+}
---
+++ tests/Docs/BrDocs/CpfTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Nscps\Ds\Tests\Docs\BrDocs;
+
+use Nscps\Ds\Docs\BrDocs\Cpf;
+use Nscps\Ds\Tests\AbstractTestCase;
+
+class CpfTest extends AbstractTestCase
+{
+
+    /**
+     * @return array
+     */
+    public function invalidPatternProvider()
+    {
+        return [
+            [''],
+            ['abc.def.ghi-jk'],
+            ['000.000.000.00'],
+            ['111-111-111-11'],
+            ['222x222x222x22'],
+            ['333 333 333 33'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidCpfProvider()
+    {
+        return [
+            // 000.000.000-00, ..., 999.999.999-99 are invalid.
+            ['000.000.000-00'],
+            ['111.111.111-11'],
+            ['222.222.222-22'],
+            ['333.333.333-33'],
+            ['444.444.444-44'],
+            ['555.555.555-55'],
+            ['666.666.666-66'],
+            ['777.777.777-77'],
+            ['888.888.888-88'],
+            ['999.999.999-99'],
+
+            // c1 is invalid
+            ['484.546.920-12'],
+            ['572.933.420-13'],
+
+            // c2 is invalid
+            ['484.546.920-00'],
+            ['484.546.920-01'],
+            ['572.933.420-62'],
+            ['572.933.420-64'],
+
+            // c1 and c2 are invalid
+            ['484.546.920-13'],
+            ['572.933.420-74'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function validCpfProvider()
+    {
+        return [
+            ['484.546.920-02'],
+            ['572.933.420-63'],
+            ['114.484.530-03'],
+            ['957.208.010-57'],
+            ['580.320.780-29'],
+            ['586.567.070-00'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidPatternProvider
+     * @param string $str
+     */
+    public function shouldThrowExceptionWhenThePatternIsInvalid($str)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The CPF must contains 11 digits or match the following pattern: "999.999.999-99".');
+
+        $cpf = new Cpf($str);
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidCpfProvider
+     * @param string $str
+     */
+    public function shouldThrowExceptionWhenTheCpfIsInvalid($str)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid CPF.');
+
+        $cpf = new Cpf($str);
+    }
+
+    /**
+     * @test
+     * @dataProvider validCpfProvider
+     * @param string $str
+     */
+    public function testToString($str)
+    {
+        $cpf = new Cpf($str);
+
+        $this->assertEquals($str, (string) $cpf);
+    }
+
+    /**
+     * @test
+     * @dataProvider validCpfProvider
+     * @param string $str
+     */
+    public function testGetRaw($str)
+    {
+        $cpf = new Cpf($str);
+
+        $expected = preg_replace('/[^0-9]/', '', $str);
+
+        $this->assertEquals($expected, $cpf->getRaw());
+    }
+
+    /**
+     * @test
+     * @dataProvider validCpfProvider
+     * @param string $str
+     */
+    public function testGetFormatted($str)
+    {
+        $cpf = new Cpf($str);
+
+        $this->assertEquals($str, $cpf->getCpf());
+    }
+
+}
