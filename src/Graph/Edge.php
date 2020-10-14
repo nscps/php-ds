74022b6e47a5dfbd4bc5cd6c9441df3bcc1cc1f8
Nilo Soares <nilosoares@gmail.com>
2020-10-14 16:40:23 -0300

    Add the Edge class

---
+++ src/Graph/Edge.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Nscps\Ds\Graph;
+
+class Edge implements EdgeInterface
+{
+
+    /**
+     * @var NodeInterface
+     */
+    private NodeInterface $from;
+
+    /**
+     * @var NodeInterface
+     */
+    private NodeInterface $to;
+
+    /**
+     * @var int|mixed
+     */
+    private $weight;
+
+    /**
+     * Edge constructor.
+     * @param NodeInterface $from
+     * @param NodeInterface $to
+     * @param mixed $weight
+     */
+    public function __construct(NodeInterface $from, NodeInterface $to, $weight = 0)
+    {
+        $this->from = $from;
+        $this->to = $to;
+        $this->weight = $weight;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFrom(): NodeInterface
+    {
+        return $this->from;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTo(): NodeInterface
+    {
+        return $this->to;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+}
